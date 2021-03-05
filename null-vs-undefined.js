737743806c82c1cb72d56bccd58adcf757458836
tarunmitra <tarunmitra26@gmail.com>
2021-03-05 12:40:25 +0600

    add

---
+++ null-vs-undefined.js
@@ -0,0 +1,32 @@
+// undefined ki jonne hoi
+
+// let pakhi;
+// console.log(pakhi);
+
+// return nah korle 
+/*
+function add(num1, num2){
+   console.log(num1 + num2);
+ }
+const result = add(13, 82);
+console.log(result);
+*/
+
+/*
+function add(num1, num2){
+   console.log(num1, num2);
+ }
+const result = add(13);
+console.log(result);
+*/
+
+// const premik = {name:"smart dude", phone:54378};
+// console.log(premik.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+// undefined hoce negative bha false
+
+// null mane holo khali akhne kisu nai 
+// null devloper ra set kore dai je okhane kisu nai
