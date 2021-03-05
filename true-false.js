c757c443e68b05c3059451fd4bfd5d2cff596bc9
tarunmitra <tarunmitra26@gmail.com>
2021-03-05 12:13:01 +0600

    add

---
+++ true-false.js
@@ -0,0 +1,52 @@
+//  const name = "0";
+// const age = 4;
+// je kono sonkha jodi value hoi sodu zero bade tahole true r zero hole false hbe
+// const age = 0;
+// const name = "Solaiman"
+//  const name = ""  // tring er vitor kisu nah likle false
+// const name = " " // tring er vitor space dile true mane er vitor kisu akta ace
+// const name = "0"  // abr zero double quotetion er vitor thkle true
+
+/*
+if(name){
+  console.log("condition is true");
+}else{
+  console.log("condition is false");
+}
+*/
+
+// summary: je kono sonkha is truety other then zero chara je kono string is true other then emthy string chara
+
+// aro kisu ashe jar mardhome false hote pare
+
+// let name   undefined
+// let name = null;
+// let name = NaN;  //NaN: not a number mane ata number nah
+// let name = false
+/*
+console.log(name);
+if(name){
+ console.log("condition is true");
+}else{
+ console.log("condition is false");
+}
+*/
+
+let name = "4";
+if(name || name == 0){
+ console.log("condition is true");
+}else{
+ console.log("condition is false");
+}
+
+// Falsy value:
+
+// false
+// 0
+// ""
+// undefined
+// null
+
+// Truthy value:
+
+// true, '0', ' ', [], {}
