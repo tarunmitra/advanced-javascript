75c4f0b1abd5d7dd6ac3cdca3708f571b33a6dc6
tarunmitra <tarunmitra26@gmail.com>
2021-03-06 06:38:12 +0600

    add

---
+++ scope.js
@@ -0,0 +1,16 @@
+// function sum(first, second){
+//   let result = first + second;
+//   return result;
+// }
+// const output = sum(3, 7);
+// console.log(output);
+
+
+
+function sum(first, second){
+  let result = first + second;
+  console.log(result);
+  return result;
+}
+const output = sum(3, 7);
+console.log(output);
